02a0b2067bb7371bff00767c70a950d45bb10f8c
dodikk <dodikk88.reg@gmail.com>
2015-12-22 03:06:39 +0200

    [GUI] Injecting viewModel to SearchVC

--- app/iWebCrawler/iWebCrawler/AppDelegate.m
+++ app/iWebCrawler/iWebCrawler/AppDelegate.m
@@ -8,38 +8,54 @@
 
 #import "AppDelegate.h"
 
-@interface AppDelegate ()
 
+
+#import "WCSettingsLocalizerApple.h"
+#import "WCSearchVC.h"
+
+
+@interface AppDelegate ()
 @end
+
 
 @implementation AppDelegate
 
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+-(BOOL)application:(UIApplication *)application
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    WCSearchModelImpl* model = [WCSearchModelImpl new];
+    WCSettingsLocalizerApple* localizer = [WCSettingsLocalizerApple new];
+    WCSettingsStatePOD* defaultSettings = [WCSettingsStatePOD new];
+    {
+        defaultSettings.maxThreadCount = 1;
+        defaultSettings.maxWebPageCount = 1;
+        defaultSettings.searchTerm = nil;
+        defaultSettings.rootUrlForSearch = nil;
+    }
+    
+    WCSettingsVMImpl* settingsVM =
+        [[WCSettingsVMImpl alloc] initWithDefaultState: defaultSettings
+                                                 model: model
+                                             localizer: localizer];
+    
+    UIStoryboard* defaultBoard = [UIStoryboard storyboardWithName: @"Main"
+                                                           bundle: nil];
+    
+    UITabBarController* tabBar = (UITabBarController*)[defaultBoard instantiateInitialViewController];
+    
+    WCSearchVC* searchVC = (WCSearchVC*)tabBar.viewControllers[0];
+    {
+        searchVC.viewModel = settingsVM;
+    }
+    
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.rootViewController = tabBar;
+    
+    // TODO : inject model to WCReportVC
+    
     return YES;
-}
-
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 @end
